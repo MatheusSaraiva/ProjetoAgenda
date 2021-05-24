f7cd253910236d10d6e68d3cbc7f35ba86428761
Matheus Saraiva <saraivarego@gmail.com>
2021-05-24 18:09:22 -0300

    'Paginação'

--- contatos/views.py
+++ contatos/views.py
@@ -1,15 +1,25 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import Contato
+from django.core.paginator import Paginator
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 20)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
 
 def ver_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
-    return render(request, 'contatos/ver_contato.html', {
-        'contato': contato
-    })
+    try:
+        contato = Contato.objects.get(id=contato_id)
+        return render(request, 'contatos/ver_contato.html', {
+            'contato': contato
+        })
+    except Contato.DoesNotExist as e:
+        raise Http404()
 
